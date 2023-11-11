12b72f4f400f665fbbd85fb720df82d7dad9da28
Sakhobjon <saxobjon7474@gmail.com>
2023-11-12 04:30:06 +0900

    +버튼 dialog 뜨게 해 봤어요~

--- app/src/main/java/com/ssuandroid/my_parttime/HomeFragment.java
+++ app/src/main/java/com/ssuandroid/my_parttime/HomeFragment.java
@@ -1,6 +1,10 @@
 package com.ssuandroid.my_parttime;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -9,7 +13,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 public class HomeFragment extends Fragment {
+
+    // alertDialog 변수를 클래스 레벨로 선언
+    private AlertDialog alertDialog;
 
     @Nullable
     @Override
@@ -17,9 +26,54 @@ public class HomeFragment extends Fragment {
         setHasOptionsMenu(true);
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-    }
+        Button button = view.findViewById(R.id.PlusButton);
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // LayoutInflater를 사용하여 커스텀 다이얼로그 레이아웃을 가져옵니다.
+                LayoutInflater inflater = getLayoutInflater();
+                View dialogView = inflater.inflate(R.layout.dialogfragment_codeinput, null);
+
+                // AlertDialog를 생성하고 설정합니다.
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(requireActivity());
+                alertDialogBuilder.setView(dialogView);
+
+                // 다이얼로그를 생성합니다.
+                alertDialog = alertDialogBuilder.create();
+
+                // 다이얼로그 안의 버튼을 찾습니다.
+                Button btnConfirm = dialogView.findViewById(R.id.codeinputButton);
+                Button btnCancel = dialogView.findViewById(R.id.codecloseButton);
+
+                // '확인' 버튼 클릭 시의 동작
+                btnConfirm.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // 확인 버튼을 눌렀을 때의 동작
+                        // 원하는 동작을 여기에 추가
+                        alertDialog.dismiss(); // 다이얼로그 닫기
+                        alertDialog = null; // 다이얼로그 변수를 null로 설정
+                    }
+                });
+
+                // '취소' 버튼 클릭 시의 동작
+                btnCancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // 취소 버튼을 눌렀을 때의 동작
+                        alertDialog.dismiss(); // 다이얼로그 닫기
+                        alertDialog = null; // 다이얼로그 변수를 null로 설정
+                    }
+                });
+
+                // 다이얼로그를 보여줍니다.
+                alertDialog.show();
+            }
+        });
+    }
 }
