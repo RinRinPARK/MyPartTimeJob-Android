bb1abf5d9b3f3663319404456b35b62c6010286b
yminjuu <ymj0580@naver.com>
2023-11-14 01:08:49 +0900

    기타 수정사항 및 다이얼로그 연결

--- app/src/main/java/com/ssuandroid/my_parttime/DaetaFragment.java
+++ app/src/main/java/com/ssuandroid/my_parttime/DaetaFragment.java
@@ -15,7 +15,7 @@ public class DaetaFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         setHasOptionsMenu(true);
-        return inflater.inflate(R.layout.fragment_alba_home, container, false);
+        return inflater.inflate(R.layout.fragment_daeta, container, false);
     }
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
--- app/src/main/java/com/ssuandroid/my_parttime/HomeFragment.java
+++ app/src/main/java/com/ssuandroid/my_parttime/HomeFragment.java
@@ -10,75 +10,36 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-public class HomeFragment extends Fragment {
-
-    // alertDialog 변수를 클래스 레벨로 선언
-    private AlertDialog alertDialog;
-    private AlertDialog hourlyRateDialog;
+public class HomeFragment extends Fragment implements View.OnClickListener{
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        Button newAlbaBtn = (Button) view.findViewById(R.id.PlusButton);
+        newAlbaBtn.setOnClickListener(this);
         setHasOptionsMenu(true);
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Button plusButton = view.findViewById(R.id.PlusButton); //homefragment에서 알바 추가 버튼
+    }
 
-        plusButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // LayoutInflater를 사용하여 커스텀 다이얼로그 레이아웃을 가져옵니다.
-                LayoutInflater inflater = getLayoutInflater();
-                View dialogView = inflater.inflate(R.layout.dialogfragment_codeinput, null);
-                View hourlyRateDialogView = inflater.inflate(R.layout.dialogfragment_hourlyrate, null);
-
-                // AlertDialog를 생성하고 설정합니다.
-                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(requireActivity());
-                alertDialogBuilder.setView(dialogView);
-
-                //hourlyRateDialog를 생성하고 설정합니다.
-                AlertDialog.Builder hourlyRateDialogBuilder = new AlertDialog.Builder(requireActivity());
-                hourlyRateDialogBuilder.setView(dialogView);
-
-                // 다이얼로그를 생성합니다.
-                alertDialog = alertDialogBuilder.create();
-                hourlyRateDialog = hourlyRateDialogBuilder.create();
-
-                // 다이얼로그 안의 버튼을 찾습니다.
-                Button btnConfirm = dialogView.findViewById(R.id.codeinputButton);
-                Button btnCancel = dialogView.findViewById(R.id.codecloseButton);
-
-                // '입력' 버튼 클릭 시의 동작
-                btnConfirm.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        hourlyRateDialog.dismiss();
-                        hourlyRateDialog.show();
-                    }
-                });
-
-                // '취소' 버튼 클릭 시의 동작
-                btnCancel.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        // 취소 버튼을 눌렀을 때의 동작
-                        alertDialog.dismiss(); // 다이얼로그 닫기
-                        alertDialog = null; // 다이얼로그 변수를 null로 설정
-                    }
-                });
-
-                // 다이얼로그를 보여줍니다.
-                alertDialog.show();
-            }
-        });
+    @Override
+    public void onClick(View v) {
+        if (v.getId()==R.id.PlusButton){
+            Log.d("ymj", "here");
+            CodeInputDialogFragment codeInputDialogFragment = new CodeInputDialogFragment();
+            codeInputDialogFragment.show(getActivity().getSupportFragmentManager(),"HOME_TAG");
+        }
     }
 }
