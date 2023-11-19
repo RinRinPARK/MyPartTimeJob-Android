9ddd43a3fac22cb4a16134c115ad184a3b0a3d94
yminjuu <ymj0580@naver.com>
2023-11-19 21:49:02 +0900

    임시커밋

--- app/src/main/java/com/ssuandroid/my_parttime/AlbaHomeFragment.java
+++ app/src/main/java/com/ssuandroid/my_parttime/AlbaHomeFragment.java
@@ -7,6 +7,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,13 +16,52 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-public class AlbaHomeFragment extends Fragment{
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+
+public class AlbaHomeFragment extends Fragment implements View.OnClickListener{
+    //근무 목록을 띄울 recyclerView
+    public RecyclerView recyclerViewWorkLog;
+    //recyclerView가 필요로 하는 adapter
+    public RecyclerView.Adapter adapter_workLog;
+    //근무 목록을 가져올 리스트
+    private ArrayList<String> workLogList;
+    FirebaseFirestore db;
+    Button workAddBtn;
+    Button moveToDaetaCalendar;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_alba_home, container, false);
         setHasOptionsMenu(true);
+
+        //db 연결 전 테스트용
+        ArrayList<String> workLog= new ArrayList<>();
+        workLog.add("19,240");
+        workLog.add("19,240");
+        workLog.add("19,240");
+        workLog.add("19,240");
+        workLog.add("19,240");
+        workLog.add("19,240");
+        workLog.add("19,240");
+        workLog.add("19,240");
+
+        //recyclerView 사용을 위한 초기 작업
+        recyclerViewWorkLog = (RecyclerView) view.findViewById(R.id.workLogRecyclerView);
+        recyclerViewWorkLog.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        adapter_workLog = new WorkLogAdapter(workLog); //임시로 만들어둔 데이터를 전옴
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerViewWorkLog.setLayoutManager(layoutManager);
+        recyclerViewWorkLog.setAdapter(adapter_workLog);
+
+        //버튼에 리스너 달아주기
+        workAddBtn = (Button) view.findViewById(R.id.inputWorkedTimeBtn);
+        moveToDaetaCalendar = (Button) view.findViewById(R.id.moveToDaetaCalendarBtn);
+        workAddBtn.setOnClickListener(this);
+        moveToDaetaCalendar.setOnClickListener(this);
         return view;
     }
     @Override
@@ -28,5 +69,14 @@ public class AlbaHomeFragment extends Fragment{
         super.onViewCreated(view, savedInstanceState);
     }
 
-
+    @Override
+    public void onClick(View v) {
+        if (v.getId()==R.id.inputWorkedTimeBtn){
+            WorkedTimeDialogFragment workedTimeDialogFragment = new WorkedTimeDialogFragment();
+            workedTimeDialogFragment.show(getActivity().getSupportFragmentManager(), "TAG");
+        }
+        else if (v.getId()==R.id.moveToDaetaCalendarBtn){
+            //대타 캘린더로 이동하기
+        }
+    }
 }
