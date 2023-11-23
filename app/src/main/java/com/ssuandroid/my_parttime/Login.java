77a96fb3d000b78d297cdb45228c1a1e2d87f395
Sakhobjon <saxobjon7474@gmail.com>
2023-11-23 19:52:34 +0900

    Login1

---
+++ app/src/main/java/com/ssuandroid/my_parttime/Login.java
@@ -0,0 +1,14 @@
+package com.ssuandroid.my_parttime;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
