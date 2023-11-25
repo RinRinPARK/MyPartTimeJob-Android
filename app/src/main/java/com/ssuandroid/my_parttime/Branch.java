4f648ea9034b7d4fd48058bb9a5f2c0f02c39b8b
yminjuu <ymj0580@naver.com>
2023-11-26 02:37:15 +0900

    daeta클래스 수정 및 daetafragment 구현 중.. 신청 버튼만 남음

--- app/src/main/java/com/ssuandroid/my_parttime/Branch.java
+++ app/src/main/java/com/ssuandroid/my_parttime/Branch.java
@@ -6,20 +6,22 @@ import com.google.firebase.firestore.ServerTimestamp;
 // 지점 관련 클래스
 public class Branch {
     // 지점 이름
-    private String name;
+    private String branchName;
     // 지점 참여 코드
     private String participationCode;
 
     @ServerTimestamp
     private Timestamp timestamp;
-
-    public Branch(String name, String participationCode) {
-        this.name = name;
+    public Branch(){
+        //no-argument constructor
+    }
+    public Branch(String branchName, String participationCode) {
+        this.branchName = branchName;
         this.participationCode = participationCode;
     }
 
-    public String getName() {
-        return name;
+    public String getbranchName() {
+        return branchName;
     }
 
     public String getParticipationCode() {
--- app/src/main/java/com/ssuandroid/my_parttime/DaetaDescriptionDialogFragment.java
+++ app/src/main/java/com/ssuandroid/my_parttime/DaetaDescriptionDialogFragment.java
@@ -1,16 +1,30 @@
 package com.ssuandroid.my_parttime;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
 
-public class DaetaDescriptionDialogFragment extends DialogFragment {
+import java.text.SimpleDateFormat;
+
+public class DaetaDescriptionDialogFragment extends DialogFragment implements View.OnClickListener {
+    private Daeta daeta;
+    int position;
     public DaetaDescriptionDialogFragment() {
+    }
+
+    public DaetaDescriptionDialogFragment(Daeta daeta, int position){
+        this.daeta= daeta;
+        this.position= position;
     }
 
     public static DaetaDescriptionDialogFragment getInstance() {
@@ -18,9 +32,74 @@ public class DaetaDescriptionDialogFragment extends DialogFragment {
         return e;
     }
 
+    //daetafragment에 데이터 넘겨주기 위한 인터페이스
+    public interface DaetaInterfacer1 {
+        void onDescriptionApplication(Daeta daeta, int position); //추상 메소드
+    }
+
+    private DaetaInterfacer1 daetaInterfacer1;
+    public void setDaetaInterfacer1(DaetaInterfacer1 daetaInterfacer1){
+        this.daetaInterfacer1= daetaInterfacer1;
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.dialogfragment_daetadescription, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View v, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(v, savedInstanceState);
+
+        TextView branchName = (TextView) v.findViewById(R.id.description_branchName);
+        TextView date = (TextView) v.findViewById(R.id.description_date);
+        TextView time = (TextView) v.findViewById(R.id.description_time);
+        TextView dayWage = (TextView) v.findViewById(R.id.description_dayWage);
+        TextView wage = (TextView) v.findViewById(R.id.description_wage);
+        TextView description = (TextView) v.findViewById(R.id.description_description);
+
+        Button closeButton = (Button) v.findViewById(R.id.description_closeBtn);
+        closeButton.setOnClickListener(this);
+        Button inputButton = (Button) v.findViewById(R.id.description_applicationBtn);
+        inputButton.setOnClickListener(this);
+
+        branchName.setText(daeta.getBranchName());
+        Log.d("ymj", daeta.getDate()+"");
+
+        time.setText(daeta.getTime());
+        dayWage.setText(Long.toString(daeta.getWage()));
+        wage.setText(Long.toString(daeta.getWage()));
+        description.setText(daeta.getDescription());
+
+        // Date를 알맞은 형식으로 변환
+        SimpleDateFormat newFormat = new SimpleDateFormat("yyyy년 M월 d일");
+        String formattedDate = newFormat.format(daeta.getDate());
+        date.setText(formattedDate);
+
+        setCancelable(false); //화면 터치 시 꺼짐을 막음
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId()==R.id.description_closeBtn){
+            Fragment fragment = getActivity().getSupportFragmentManager().findFragmentByTag("DAETA_DESCRIPTION_TAG");
+            if (fragment!=null){
+                DialogFragment dialogFragment = (DialogFragment) fragment;
+                dialogFragment.dismiss();
+            }
+        }
+        else if (v.getId()==(R.id.description_applicationBtn)){
+            if (daeta!=null){
+                daeta.setCovered();
+                daetaInterfacer1.onDescriptionApplication(daeta, position);
+                //신청 버튼
+                Fragment fragment = getActivity().getSupportFragmentManager().findFragmentByTag("DAETA_DESCRIPTION_TAG");
+                if (fragment!=null){
+                    DialogFragment dialogFragment = (DialogFragment) fragment;
+                    dialogFragment.dismiss();
+                }
+            }
+        }
     }
 }
